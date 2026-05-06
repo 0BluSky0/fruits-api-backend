cf1a77ccd2128e5e69262eca5ecc2490add4e0ae
Sepehr <sepehrmohammad07@gmail.com>
2026-05-06 12:37:39 +0100

    update app.js after installing cors package

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const fruits = require("./routes/fruits-router")
+const fruits = require("./routes/fruits-router");
 
 app.get('/', (req, res) => {
    res.send('Hello Fruity!');
 })
 
+app.use(cors())
 app.use(express.json());
 app.use('/fruits', fruits);
 
